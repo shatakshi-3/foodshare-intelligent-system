b05a8dc8c261e58cdb7e327b0c453ac8ab867a07
Shatakshi Dubey <shatakshidubey441@gmail.com>
2026-05-04 17:48:37 +0530

    cleanup

--- Food-Share-main/routes/llmMatching.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const middleware = require("../middleware/index.js");
-const llmMatchingController = require("../controllers/llmMatchingController.js");
-
-router.get("/admin/donation/ai-matches/:donationId", middleware.ensureAdminLoggedIn, llmMatchingController.viewLlmMatches);
-
-module.exports = router;
--- Food-Share-main/routes/matching.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const middleware = require("../middleware/index.js");
-const matchingController = require("../controllers/matchingController.js");
-
-router.get("/admin/donation/matches/:donationId", middleware.ensureAdminLoggedIn, matchingController.viewMatches);
-
-module.exports = router;
